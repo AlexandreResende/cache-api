32a8c71f5faa6ee9fc8de740f2c96f1f0fe6c90d
Alexandre Resende <alexandre.amres@gmail.com>
2021-10-02 11:30:42 -0300

    refactor: Update GetCacheDataCommand file

--- src/commands/GetCacheDataCommand.ts
+++ src/commands/GetCacheDataCommand.ts
@@ -5,13 +5,20 @@ import { IBaseCommand } from "./IBaseCommand";
 
 type getCacheData = { key: string };
 
+interface ICache {
+  get(key: string): object;
+  set(): void;
+}
+
 export default class GetCacheDataCommand implements IBaseCommand {
   constructor(
     private readonly events: EventEmitter,
-    private readonly cache = { get: (key: string) => { return { message: 'key gotten' } }, set: () => {} },
+    private readonly cache: ICache,
   ) {}
 
   async execute(payload: getCacheData): Promise<void | boolean> {
-    return this.events.emit(CACHE.CACHE_RETRIEVED_EVENT, this.cache.get(payload.key));
+    const cachedData = this.cache.get(payload.key);
+
+    this.events.emit(CACHE.CACHE_RETRIEVED_EVENT, cachedData);
   }
 }
