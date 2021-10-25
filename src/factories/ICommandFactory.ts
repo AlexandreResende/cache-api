2642c4e96dcc88dcc6e5347ec0b1849b485b4ed8
Alexandre Resende <alexandre.amres@gmail.com>
2021-10-25 19:56:42 -0300

    refactor: Remove command factories

--- src/factories/ICommandFactory.ts
+++
@@ -1,7 +0,0 @@
-
-import { EventEmitter } from "events";
-import { IBaseCommand } from "../commands/IBaseCommand";
-
-export interface ICommandFactory {
-  create(events: EventEmitter): Promise<IBaseCommand>;
-}
