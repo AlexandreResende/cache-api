3dd54d784d6dcc7e2e22d287a38153f059e874df
Alexandre Resende <alexandre.amres@gmail.com>
2021-10-26 19:41:39 -0300

    refactor: Update routes file to be more dynamic

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,51 +1,28 @@
-import cors from 'cors';
+import { Router } from 'express';
+import { container, injectable, injectAll, Lifecycle, scoped } from 'tsyringe';
+import Controller from '../controllers/Controller';
+import { logger } from '../Logger';
 
-import { Router } from "express";
-import { API } from '../Environment';
-import { expressHandler } from "../controllers/ExpressHandler";
-import { GetCacheDataController } from "../controllers/cache/GetCacheDataController";
-import { GetAllStoredKeysController } from '../controllers/cache/GetAllStoredKeysController';
-import { CreateCacheEntryController } from '../controllers/cache/CreateCacheEntryController';
-import { DeleteCacheEntryController } from '../controllers/cache/DeleteCacheEntryController';
-import { DeleteAllCacheEntriesController } from '../controllers/cache/DeleteAllCacheEntriesController';
-import { UpdateCacheEntryController } from '../controllers/cache/UpdateCacheEntryController';
+export type HttpVerb = 'get' | 'put' | 'post' | 'delete';
 
-export class Routes {
-  public routes: Router;
+@scoped(Lifecycle.ResolutionScoped)
+@injectable()
+class CustomRouter {
+  public readonly router: Router;
 
-  constructor() {
-    this.routes = Router();
+  constructor(@injectAll('Controller') private readonly controllers: Controller[]) {
+    this.router = Router();
 
-    this.configRoutes();
+    this.controllers.forEach((controller) => {
+      logger.info(`Route ${controller.method} ${controller.path} created`);
+
+      this.router[controller.method](controller.path, controller.handleRequest);
+    });
+
+    this.router.use('*', (req) => {
+      throw new Error(`Cannot ${req.method} ${req.path}`);
+    });
   }
+}
 
-  private configRoutes(): void {
-    const options: cors.CorsOptions = {
-      allowedHeaders: [
-        'Origin',
-        'X-Requested-With',
-        'Content-Type',
-        'Accept',
-        'X-Access-Token',
-      ],
-      credentials: true,
-      methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-      origin: `http://localhost:${API.PORT}`,
-      preflightContinue: false,
-    };
-
-    // needs improvement since the more the api grows the bigger this will be
-    this.routes
-      .use(cors(options))
-      .get("/cache/:key", expressHandler(new GetCacheDataController().handleRequest))
-      .get("/cache", expressHandler(new GetAllStoredKeysController().handleRequest))
-      .post("/cache", expressHandler(new CreateCacheEntryController().handleRequest))
-      .delete("/cache/:key", expressHandler(new DeleteCacheEntryController().handleRequest))
-      .delete("/cache", expressHandler(new DeleteAllCacheEntriesController().handleRequest))
-      .put("/cache", expressHandler(new UpdateCacheEntryController().handleRequest));
-  }
-
-  public getRoutes(): Router {
-    return this.routes;
-  }
-};
+export default container.resolve(CustomRouter).router;
