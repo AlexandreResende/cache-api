ece820851f706f6690f68c96fe5db9927303767d
Alexandre Resende <alexandre.amres@gmail.com>
2021-10-29 08:28:51 -0300

    refactor: Create getHandlers method  for routes

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { RequestHandler, Router } from 'express';
 import { container, injectable, injectAll, Lifecycle, scoped } from 'tsyringe';
 import Controller from '../controllers/Controller';
 import { logger } from '../Logger';
@@ -16,12 +16,24 @@ class CustomRouter {
     this.controllers.forEach((controller) => {
       logger.info(`Route ${controller.method} ${controller.path} created`);
 
-      this.router[controller.method](controller.path, controller.handleRequest);
+      this.router[controller.method](controller.path, this.getHandlers(controller));
     });
 
     this.router.use('*', (req) => {
       throw new Error(`Cannot ${req.method} ${req.path}`);
     });
+  }
+
+  private getHandlers(controller: Controller): RequestHandler[] {
+    const handler: RequestHandler = async (req, res, next) => {
+      try {
+        await controller.handleRequest(req, res);
+      } catch (error) {
+        next(error);
+      }
+    };
+
+    return [ handler ];
   }
 }
 
