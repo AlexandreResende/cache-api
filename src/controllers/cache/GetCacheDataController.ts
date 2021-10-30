1e5d42f719f13cb21a10509feb4a9b58e8cbf5c3
Alexandre Resende <alexandre.amres@gmail.com>
2021-10-30 17:32:18 -0300

    refactor: Remove unnecessary logs

--- src/controllers/cache/GetCacheDataController.ts
+++ src/controllers/cache/GetCacheDataController.ts
@@ -13,26 +13,20 @@ export class GetCacheDataController extends Controller {
   constructor(
     @inject('GetCacheDataCommand') private readonly getCacheDataCommand: GetCacheDataCommand
   ) {
-    super('get', '/cache');
+    super('get', '/cache/:key');
   }
 
   async handleRequest(req: Request, res: Response): Promise<void> {
-    console.log('INSIDE CONTROLLER!!!!!');
     const cacheDataRetrieved = (data: object) => {
-      console.log('response2222', data);
       return HttpResponseHandler.sendSuccess(res, data);
     };
     const cacheDataNotFound = (data: object) => {
-      console.log('response2222', data);
       return HttpResponseHandler.sendCreated(res, data);
     };
-    console.log('+++++++++++=');
+
     const events = new EventEmitter();
     events.on(CACHE.CACHE_RETRIEVED_EVENT, cacheDataRetrieved);
     events.on(CACHE.CACHE_NOT_FOUND_EVENT, cacheDataNotFound);
-    console.log('==============');
-
-    console.log(this.getCacheDataCommand);
 
     await this.getCacheDataCommand.execute(events, { key: req.params.key });
   }
